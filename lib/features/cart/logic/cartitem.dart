770d0a4f25f2c1fdcc55de86499e43451a416f18
Ahmed0mar2 <hamada.ali952.ao@gmail.com>
2025-05-03 15:29:44 +0300

    products screen

--- lib/features/cart/logic/cartitem.dart
+++
@@ -1,8 +0,0 @@
-import 'package:volt_market/features/products/models/models.dart';
-
-class CartItem {
-  final Product product;
-  int quantity;
-
-  CartItem({required this.product, this.quantity = 1});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:volt_market/core/helper/sharedpref_helper.dart';
 import 'package:volt_market/core/routing/app_router.dart';
 import 'package:volt_market/core/routing/my_routes.dart';
 import 'package:volt_market/core/volt_market.dart';
-import 'package:volt_market/features/cart/logic/cartprovider.dart';
 import 'package:volt_market/firebase_options.dart';
 
 void main() async {
@@ -33,10 +31,5 @@ void main() async {
     }
   }
 
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => CartProvider(),
-      child: VoltMarket(appRouter: AppRouter(), initialRoute: initialRoute),
-    ),
-  );
+  runApp(VoltMarket(appRouter: AppRouter(), initialRoute: initialRoute));
 }
